8365ba44a6c6330b5987345249435ab06efb5199
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2022-05-26 08:52:58 +0700

    feat: enable configuration properties and upgrade some dependencies to the latest version

---
+++ src/main/java/com/example/smarterbackend/SmarterConfigurationProperties.java
@@ -0,0 +1,7 @@
+package com.example.smarterbackend;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("smarter")
+public record SmarterConfigurationProperties() {
+}
